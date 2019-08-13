37d0acbd13134f240c033404b7bf01f22efd2b29
vrx_lasiqueira@uolinc.com <leandroaps@gmail.com>
2019-08-13 14:03:53 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
